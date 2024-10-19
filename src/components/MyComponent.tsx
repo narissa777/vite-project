f371ffd67c807c3e7e6ad23c6a736071bed7c373
narissa777 <blabla.ata@gmail.com>
2024-10-19 17:58:31 +0300

    Fix import from @twa-dev/sdk

--- src/components/MyComponent.tsx
+++ src/components/MyComponent.tsx
@@ -1,15 +1,18 @@
 import React, { useEffect } from 'react';
-import { SomeFunction } from '@twa-dev/sdk';
+import WebApp from '@twa-dev/sdk';
 
 const MyComponent: React.FC = () => {
   useEffect(() => {
-    // SDK'yı burada kullanabilirsiniz
-    SomeFunction();
+    // SDK'nın hazır olduğunu kontrol etmek
+    WebApp.ready();
   }, []);
 
   return (
     <div>
       <h1>My Component</h1>
+      <button onClick={() => WebApp.showAlert('Hello World!')}>
+        Show Alert
+      </button>
     </div>
   );
 };
